ca4a13d13572eb914cd64493e5d572207f45bbe6
mrkvon <michal.salajka@email.cz>
2015-10-04 23:29:10 +0200

    added searching for users with similar tags to logged in user

--- routes/search.js
+++ routes/search.js
@@ -2,11 +2,39 @@
 
 var express = require('express');
 var router = express.Router();
+var database = require('../services/data');
 
-router.all('*', function(req, res, next) {
-  var err = new Error('TODO router for ' + req.originalUrl);
-  err.status = 404;
-  next(err);
+router.all('*', function (req, res, next) {
+  var sessUser = req.session.user;
+  if(sessUser.logged === true) return next();
+  var err = new Error('not logged in');
+  throw err;
+  //return next(err);
+});
+
+router.get('/', function (req, res, next) {
+  var sessUser = req.session.user;
+  return res.render('search', {session: sessUser});
+});
+
+router.post('/', function (req, res, next) {
+  var sessUser = req.session.user;
+  var submit = req.body.search;
+
+  if(submit ==='users with my tags') return database.search.usersWithTagsOfUser({username: sessUser.username})
+    .then(function (results) {
+      return res.format({
+        'text/html': function () {
+          return res.render('users-with-my-tags', {data: results, session: sessUser})
+        },
+        'application/json': function () {
+          return res.send(results);
+        }
+      });
+    })
+    .then(null, function (err) {
+      return next(err);
+    });
 });
 
 module.exports = router;
