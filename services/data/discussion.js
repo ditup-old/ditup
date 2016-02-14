03a6bd793b9ee23b44ed473a8c97f7433458975b
mrkvon <michal.salajka@email.cz>
2016-02-14 01:50:14 +0000

    database discussion functions test & implementation create, read, delete

--- services/data/discussion.js
+++ services/data/discussion.js
@@ -4,8 +4,56 @@
 module.exports = function (db) {
   var discussion = {};
   
-  discussion.create = function (discussion) {
-    return db.query('');
+  discussion.create = function (dscn) {
+    
+    var isDataComplete = !!dscn.topic && !!dscn.creator;
+
+    if(!isDataComplete) {
+      return Promise.reject('incomplete data');
+    }
+    else {
+      var query = 'INSERT {topic: @topic, creator: @creator, created: @created, posts: []} IN discussions RETURN NEW._key';
+      var params = {topic: dscn.topic, creator: dscn.creator, created: dscn.created || Date.now()};
+
+      return db.query(query, params)
+        .then(function (cursor) {
+          return cursor.all();
+        })
+        .then(function (arrayId) {
+          return {id: arrayId[0]};
+        })
+        .then(null, function (err) {
+          return Promise.reject(err);
+        });
+    }
+  };
+
+  discussion.read = function (id) {
+    var query = 'FOR d IN discussions FILTER d._key == @id RETURN d';
+    var params = {id: id};
+
+    return db.query(query, params)
+      .then(function (cursor) {
+        return cursor.all();
+      })
+      .then(function (discs) {
+        if(discs.length === 1) {
+          return discs[0];
+        }
+        else if(discs.length === 0) {
+          throw new Error(404);
+        }
+        else {
+          throw new Error('duplicate discussion id. this should never happen.');
+        }
+      });
+  };
+
+  discussion.delete = function (id) {
+    var query = 'FOR d IN discussions FILTER d._key == @id REMOVE d IN discussions';
+    var params = {id: id};
+
+    return db.query(query, params);
   };
 
   return discussion;
--- test/unit/services-data-discussion.js
+++ test/unit/services-data-discussion.js
@@ -17,16 +17,99 @@ describe('database/discussion', function () {
     context('when data is incomplete', function () {
       var incompleteData = {
       };
-      it('should reject a promise with a proper error message', function () {
-        return expect(discussion.create(incompleteData)).to.eventually.be.rejectedWith('missing data');
+      it('should return a promise and reject it with a proper error message', function () {
+        return expect(discussion.create(incompleteData)).to.eventually.be.rejectedWith('incomplete data');
       });
     });
 
-    context('when data is complete', function () {});
+    context('when data is complete', function () {
+      var completeData = {
+        topic: 'discussion topic',
+        creator: 'test1',
+        created: Date.now()
+      };
+
+      it('should save a disussion to the database, return a promise and resolve it with the discussion id', function () {
+        return expect(discussion.create(completeData)).to.eventually.have.property('id');
+      });
+    });
   });
-  describe('read', function () {});
+
+  describe('read(id)', function () {
+    context('when discussion with :id exists', function () {
+      var completeData = {
+        topic: 'discussion topic',
+        creator: 'test1',
+        created: Date.now()
+      };
+      var existentId;
+      beforeEach(function(done) {
+        discussion.create(completeData)
+          .then(function (obj) {
+            existentId = obj.id;
+          })
+          .then(function () {
+            done();
+          }, done);
+      });
+      it('should return the discussion object (including some posts)', function () {
+        return Promise.all([
+          expect(discussion.read(existentId)).to.eventually.be.fulfilled,
+          expect(discussion.read(existentId)).to.eventually.have.property('topic'),
+          expect(discussion.read(existentId)).to.eventually.have.property('creator'),
+          expect(discussion.read(existentId)).to.eventually.have.property('created'),
+          expect(discussion.read(existentId)).to.eventually.have.property('posts')
+        ]);
+      });
+      afterEach(function(done) {
+        db.query('FOR d IN discussions REMOVE d IN discussions')
+          .then(function () {
+            done();
+          }, done);
+      });
+    });
+    context('when discussion with :id doesn\'t exist', function () {
+      var nonexistentId = String(211111111111111);
+      it('should return a promise and reject it with a proper error message', function () {
+        return Promise.all([
+          expect(discussion.read(nonexistentId)).to.eventually.be.rejectedWith(404)
+        ]);
+      });
+    });
+  });
   describe('update', function () {})
-  describe('delete', function () {});
+  describe('delete(id)', function () {
+    context('when discussion with :id exists', function () {
+      var completeData = {
+        topic: 'discussion topic',
+        creator: 'test1',
+        created: Date.now()
+      };
+      var existentId;
+      beforeEach(function(done) {
+        discussion.create(completeData)
+          .then(function (obj) {
+            existentId = obj.id;
+          })
+          .then(done, done);
+      });
+      it('should delete the discussion from the database and be fulfilled', function () {
+        return expect(discussion.delete(existentId)).to.eventually.be.fulfilled;
+      });
+      afterEach(function(done) {
+        db.query('FOR d IN discussions REMOVE d IN discussions')
+          .then(function () {
+            done();
+          }, done);
+      });
+    });
+    context('when discussion with :id doesn\'t exist', function () {
+      var nonexistentId = String(211111111111111);
+      it('should return a promise and reject it with a proper error message', function () {
+        return expect(discussion.delete(nonexistentId)).to.eventually.be.rejectedWith(404);
+      });
+    });
+  });
   describe('addMessage', function () {});
   describe('removeMessage', function () {});
   describe('addTag', function () {});
@@ -34,5 +117,6 @@ describe('database/discussion', function () {
   describe('follow', function () {});
   describe('unfollow', function () {});
   describe('hide', function () {});
+  describe('readTagDiscussions', function () {});
 
 });
