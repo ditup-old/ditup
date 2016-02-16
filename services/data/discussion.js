20b06537fcbdb7e7bcd6973a22f2e2f9515a5a8c
mrkvon <michal.salajka@email.cz>
2016-02-16 03:14:28 +0000

    database discussion add and update post

--- services/data/discussion.js
+++ services/data/discussion.js
@@ -12,11 +12,15 @@ module.exports = function (db) {
       return Promise.reject('incomplete data');
     }
     else {
-      var query = 'INSERT {topic: @topic, creator: @creator, created: @created, posts: []} IN discussions RETURN NEW._key';
+      var query = 'FOR u IN users FILTER u.username == @creator INSERT {topic: @topic, creator: u._id, created: @created, posts: []} IN discussions RETURN NEW._key';
       var params = {topic: dscn.topic, creator: dscn.creator, created: dscn.created || Date.now()};
 
       return db.query(query, params)
         .then(function (cursor) {
+          var writes = cursor.extra.stats.writesExecuted;
+          if (writes === 0) throw new Error(404);
+          if (writes > 1) throw new Error('more than 1 discussion created. this should never happen');
+
           return cursor.all();
         })
         .then(function (arrayId) {
@@ -60,6 +64,74 @@ module.exports = function (db) {
         else if (writes === 1) return {success: true}
         else throw new Error('problems with removing tag (this should never happen)');
       });
+  };
+
+  discussion.addPost = function (id, post) {
+    var query = `FOR d in discussions FILTER d._key == @id
+        FOR u IN users FILTER u.username == @creator
+            LET posts = (PUSH(d.posts, {text: @text, created: @created, creator: u._id}, false))
+            UPDATE d WITH {posts: posts} IN discussions
+            RETURN LENGTH(NEW.posts)-1`;
+    var params = {id: id, creator: post.creator, created: post.created || Date.now(), text: post.text};
+
+    return db.query(query, params)
+      .then(function (cursor) {
+        var writes = cursor.extra.stats.writesExecuted;
+        if (writes === 0) throw new Error(404);
+        if (writes > 1) throw new Error('more than 1 discussion updated. this should never happen.');
+        return cursor.all();
+      })
+      .then(function (updated) {
+        return {id: updated[0]};
+      });
+  }
+  
+  discussion.updatePost = function (id, post, editable) {
+    editable = editable || false;
+
+    if(!post.user || !post.text || !post.index || !id ) return Promise.reject(400);
+    //find out if user can edit the post
+    var canEdit;
+    //if she has special rights, can edit.
+    if(editable === true) canEdit = Promise.resolve('admin');
+    else {
+        
+      //this query should resolve if user & post creator match.
+      //else it should return promise and reject it(with some error codes).
+      var canEditQuery = `FOR d IN discussions FILTER d._key == @id
+      FOR u IN users FILTER u.username == @username
+          RETURN TO_BOOL(NTH(d.posts, @index)) ? ( u._id == d.posts[@index].creator ? true : 401) : 404
+        `;
+      var canEditParams = {id: id, index: post.index, username: post.user};
+      canEdit = db.query(canEditQuery, canEditParams)
+        .then(function (cursor) {
+          return cursor.all();
+        })
+        .then(function (can) {
+          //if discussion or user was not found
+          if(can.length === 0) throw new Error(404);
+          if(can.length > 1) throw new Error(500);
+          if(can[0] === true) return 'user';
+          if(can[0] === 401) throw new Error(401);
+          if(can[0] === 404) throw new Error(404);
+          throw new Error('uncaught');
+        });
+    }
+
+    return canEdit
+      .then(function () {
+        var query = `FOR d IN discussions FILTER d._key == @id
+        UPDATE d WITH {
+            posts: UNION(
+                SLICE(d.posts, 0, @index),
+                [MERGE(d.posts[@index], {text: @text, updated: @updated})],
+                SLICE(d.posts, @index+1)
+            )
+        } IN discussions
+        RETURN NEW`;
+        var params = {id: id, text: post.text, /*user: post.user, */updated: post.updated || Date.now(), index: post.index };
+        return db.query(query, params);
+    });
   };
 
   return discussion;
--- test/unit/services-data-discussion.js
+++ test/unit/services-data-discussion.js
@@ -12,6 +12,12 @@ var db = new Database({url: config.url, databaseName: config.dbname});
 
 var discussion = require('../../services/data/discussion')(db);
 
+var completeData = {
+  topic: 'discussion topic',
+  creator: 'test1',
+  created: Date.now()
+};
+
 describe('database/discussion', function () {
   describe('create', function () {
     context('when data is incomplete', function () {
@@ -72,7 +78,7 @@ describe('database/discussion', function () {
       var nonexistentId = String(211111111111111);
       it('should return a promise and reject it with a proper error message', function () {
         return Promise.all([
-          expect(discussion.read(nonexistentId)).to.eventually.be.rejectedWith(404)
+          expect(discussion.read(nonexistentId)).to.eventually.be.rejectedWith('404')
         ]);
       });
     });
@@ -106,14 +112,16 @@ describe('database/discussion', function () {
     context('when discussion with :id doesn\'t exist', function () {
       var nonexistentId = String(211111111111111);
       it('should return a promise and reject it with a proper error message', function () {
-        return expect(discussion.delete(nonexistentId)).to.eventually.be.rejectedWith(404);
+        return expect(discussion.delete(nonexistentId)).to.eventually.be.rejectedWith('404');
       });
     });
   });
   describe('addPost', function () {
-    var post = 'this is some post text. Lorem ipsum dolor sic amet';
+    var postText = 'this is some post text. Lorem ipsum dolor sic amet';
+    var author = 'mrkvon';
+    var nonexistentAuthor = 'nonexistent-user';
 
-    context('when discussion with :id exists', function () {
+    context('when discussion with :id exists and author exists', function () {
       var existentId;
       beforeEach(function(done) {
         discussion.create(completeData)
@@ -124,20 +132,174 @@ describe('database/discussion', function () {
             done();
           }, done);
       });
+      afterEach(function(done) {
+        db.query('FOR d IN discussions REMOVE d IN discussions')
+          .then(function () {
+            done();
+          }, done);
+      });
 
       it('should add post to the database and return promise and fulfill it with post id', function () {
-        return expect(discussion.addPost({discussionId: existentId, post: post, creator: author})).to.eventually.have.property('id');
+        return expect(discussion.addPost(existentId, {text: postText, creator: author})).to.eventually.deep.equal({id: 0})
+          .then(function () {
+            return expect(discussion.addPost(existentId, {text: postText, creator: author})).to.eventually.deep.equal({id: 1});
+          });
       });
     });
 
     context('when discussion with :id doesn\'t exist', function () {
       var nonexistentId = String(211111111111111);
       it('should return promise and reject it with proper error (404)', function () {
-        return expect(discussion.delete(nonexistentId)).to.eventually.be.rejectedWith(404);
+        return expect(discussion.addPost(nonexistentId, {text: postText, creator: author})).to.eventually.be.rejectedWith('404');
+      });
+    });
+
+    context('when author doesn\'t exist', function () {
+      var existentId;
+      beforeEach(function(done) {
+        discussion.create(completeData)
+          .then(function (obj) {
+            existentId = obj.id;
+          })
+          .then(function () {
+            done();
+          }, done);
+      });
+      afterEach(function(done) {
+        db.query('FOR d IN discussions REMOVE d IN discussions')
+          .then(function () {
+            done();
+          }, done);
+      });
+      it('should return a promise and reject it with proper error (404)', function () {
+        return expect(discussion.addPost(existentId, {text: postText, creator: nonexistentAuthor})).to.eventually.be.rejectedWith('404');
       });
     });
   });
-  describe('updatePost', function () {});
+
+  describe('updatePost', function () {
+    context('when discussion doesn\'t exist', function () {
+      it('should return a promise and reject it with 404 error');
+    });
+
+    context('when discussion exists', function () {
+      //create discussion and add some posts to it
+      var completeData = {
+        topic: 'discussion topic',
+        creator: 'test1',
+        created: Date.now()
+      };
+      var existentId;
+      beforeEach(function(done) {
+        discussion.create(completeData)
+          .then(function (obj) {
+            existentId = obj.id;
+            return discussion.addPost(existentId, {creator: 'mrkvon', text: 'this is some post text'});
+          })
+          .then(function () {
+            return discussion.addPost(existentId, {creator: 'test1', text: 'this is some post text 2'});
+          })
+          .then(function () {
+            return discussion.addPost(existentId, {creator: 'mrkvon', text: 'this is some post text 3'});
+          })
+          .then(function () {
+            done();
+          }, done);
+      });
+      afterEach(function(done) {
+        db.query('FOR d IN discussions REMOVE d IN discussions')
+          .then(function () {
+            done();
+          }, done);
+      });
+    
+
+      context('when post doesn\'t exist', function () {
+        it('should return a promise and reject it with 404 error', function () {
+          return expect(discussion.updatePost(existentId, {
+              index: 5,
+              user: 'mrkvon',
+              text: 'this is a new post text2',
+              updated: Date.now()
+            })).to.eventually.be.rejectedWith('404');
+        });
+      });
+
+      context('when post was created by user && data is ok', function () {
+
+        it('should update the post and return a promise and resolve it', function () {
+          var updatedPost;
+          return expect(
+            discussion.updatePost(existentId, {
+              index: 1,
+              user: 'test1',
+              text: 'this is a new post text2',
+              updated: Date.now()
+            })
+              .then(function () {
+                return discussion.read(existentId);
+              })
+              .then(function (disc) {
+                updatedPost = disc.posts[1];
+                return updatedPost;
+              })
+          ).to.eventually.have.property('text', 'this is a new post text2')
+            .then(function () {
+              expect(updatedPost).to.have.property('creator');
+              expect(updatedPost).to.have.property('updated');
+            });
+        });
+      });
+
+      context('when access as admin and data is ok', function () {
+        it('should update the post and return a promise and resolve it', function () {
+          var updatedPost;
+          return expect(
+            discussion.updatePost(existentId, {
+              index: 1,
+              user: 'mrkvon',
+              text: 'this is a new post text2',
+              updated: Date.now()
+            }, true)
+              .then(function () {
+                return discussion.read(existentId);
+              })
+              .then(function (disc) {
+                updatedPost = disc.posts[1];
+                return updatedPost;
+              })
+          ).to.eventually.have.property('text', 'this is a new post text2')
+            .then(function () {
+              expect(updatedPost).to.have.property('creator');
+              expect(updatedPost).to.have.property('updated');
+            });
+        });
+      });
+
+      context('when allowed access and data is incomplete', function () {
+        it('should return and reject a promise with 400 (bad data)', function () {
+          return expect(
+            discussion.updatePost(existentId, {
+              index: 1,
+              user: 'mrkvon',
+              updated: Date.now()
+            }, true)
+          ).to.eventually.be.rejectedWith('400');
+        });
+      });
+
+      context('when access not allowed', function () {
+        it('should return a promise and reject it with error 401.', function () {
+          return expect(discussion.updatePost(existentId, {
+              index: 1,
+              user: 'mrkvon',
+              text: 'this is a new post text2',
+              updated: Date.now()
+            })).to.eventually.be.rejectedWith('401');
+        });
+      });
+    });
+  });
   describe('removePost', function () {});
   describe('addTag', function () {});
   describe('removeTag', function () {});
