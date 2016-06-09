b7d7e28a4d7fffaf3a1a446f57c867541951756b
mrkvon <michal.salajka@email.cz>
2016-06-09 20:08:24 +0200

    test+implement showing various lists of user's projects (following, common tags)

--- routes/user/projects.js
+++ routes/user/projects.js
@@ -8,7 +8,7 @@ module.exports = function (dependencies) {
     var username = req.params.username;
     var sessUser = req.session.user;
 
-    let involved;
+    let involved, following, commonTags;
 
     if(username === sessUser.username) {
       return data.project.userProjects(username)
@@ -16,7 +16,19 @@ module.exports = function (dependencies) {
           involved = _inv;
         })
         .then(function () {
-          return res.render('user-projects', {session: sessUser, user: {username: username}, involved: involved, following: [], suggestedByCommonTags: []});
+          return data.project.following(username);
+        })
+        .then(function (_fol) {
+          following = _fol;
+        })
+        .then(function () {
+          return data.project.projectsByTagsOfUser(username);
+        })
+        .then(function (_com) {
+          commonTags = _com;
+        })
+        .then(function () {
+          return res.render('user-projects', {session: sessUser, user: {username: username}, involved: involved, following: following, commonTags: commonTags});
         })
         .then(null, function (err) {
           return next(err);
--- test/functional/user/user-projects.js
+++ test/functional/user/user-projects.js
@@ -86,18 +86,34 @@ describe('visit /user/:username/projects', function () {
     });
 
     afterEach(logout);
+
     context('the logged user is :username', function () {
       it('should be successful', function () {
         browser.assert.success();
       });
       it('should show list of projects user is involved in (member/invited/joining)', function () {
-        browser.assert.element('.member-joining-invited-list');
-        browser.assert.text('.member-joining-invited-list .project .project-name', new RegExp('(?=.*project0)(?=.*project1)(?=.*project2)'));
-        browser.assert.attribute('.member-joining-invited-list .project-link.project-id-'+dbData.projects[0].id, 'href', new RegExp('^/project/'+dbData.projects[0].id+'.*$'));
-        browser.assert.text('.member-joining-invited-list .project-involvement', new RegExp('(?=.*joining)(?=.*invited)(?=.*member)'));
+        browser.assert.element('.involved-list');
+        browser.assert.text('.involved-list .project .project-name', new RegExp('(?=.*project0)(?=.*project1)(?=.*project2)'));
+        browser.assert.attribute('.involved-list .project-link.project-id-'+dbData.projects[0].id, 'href', new RegExp('^/project/'+dbData.projects[0].id+'.*$'));
+        browser.assert.text('.involved-list .project-involvement', new RegExp('(?=.*joining)(?=.*invited)(?=.*member)'));
       });
-      it('should show list of projects user follows');
-      it('should show list of projects which have common tags with user (sorted by amount of common tags)');
+      it('should show list of projects user follows', function () {
+        browser.assert.element('.following-list');
+        browser.assert.text('.following-list .project .project-name', new RegExp('(?=.*project1)'));
+        browser.assert.attribute('.following-list .project-link.project-id-'+dbData.projects[1].id, 'href', new RegExp('^/project/'+dbData.projects[1].id+'.*$'));
+      });
+      it('should show list of projects which have common tags with user (sorted by amount of common tags)', function () {
+        browser.assert.element('.common-tags-list');
+        browser.assert.text('.common-tags-list .project .project-name', new RegExp('(?=.*project1)'));
+        browser.assert.attribute('.common-tags-list li:first-child .project-link', 'href', new RegExp('^/project/'+dbData.projects[1].id+'.*$'));
+        browser.assert.text('.common-tags-list li:first-child .common-tagno', '2 tags');
+        let tagRegex = '';
+        for(let tg of dbData.projects[1].tags) {
+          tagRegex += '(?=.*'+tg+')';
+        }
+        browser.assert.attribute('.common-tags-list li:first-child .common-tagno', 'title', new RegExp(tagRegex));
+        browser.assert.attribute('.common-tags-list li:first-child .common-tagno', 'data-tooltip', new RegExp(tagRegex));
+      });
     });
     context('the logged user is not :username', function () {
       it('we should define what should happen');
