f9d408a8a344afcd222e12e436f5ad51284ba0a9
mrkvon <michal.salajka@email.cz>
2016-07-06 18:23:43 +0200

    starting /notifications, upgrading arangojs to v5

---
+++ test/functional/notifications/dbNotifications.js
@@ -0,0 +1,118 @@
+'use strict';
+
+module.exports = (function () {
+  const USERS = 5;
+  const TAGS = 6;
+
+  let data = {
+    "users": [
+    ],
+    "tags": [
+      {
+        "name": "tag0",
+        "description": "this is the tag0",
+        "creator": 0
+      },
+      {
+        "name": "tag1",
+        "description": "this is the tag1",
+        "creator": 0
+      },
+      {
+        "name": "tag2",
+        "description": "this is a description of the tag2",
+        "creator": 1
+      },
+      {
+        "name": "tag3",
+        "description": "this is some other description of the tag",
+        "creator": 1
+      },
+      {
+        "name": "tag4",
+        "description": "this is some other description of the tag",
+        "creator": 0
+      },
+      {
+        "name": "tag5",
+        "description": "this is some other description of the tag",
+        "creator": 0
+      }
+    ],
+    "discussions": [],
+    "challenges": [],
+    "ideas": [
+    ],
+    "projects": [
+    ],
+    "challengeCommentAuthor": [],
+    "discussionCommentAuthor": [],
+    "ideaCommentAuthor": [],
+    "projectCommentAuthor": [],
+    "userFollowChallenge": [],
+    "userFollowDiscussion": [],
+    "userFollowIdea": [
+    ],
+    "userFollowProject": [
+    ],
+    "userTag": [
+      {"user":0, "tag": 0},
+      {"user":0, "tag": 2},
+      {"user":0, "tag": 4},
+      {"user":0, "tag": 5},
+      {"user":1, "tag": 1},
+      {"user":1, "tag": 2},
+      {"user":1, "tag": 3},
+      {"user":1, "tag": 4},
+      {"user":2, "tag": 4},
+      {"user":3, "tag": 1},
+      {"user":3, "tag": 5}
+    ],
+    "discussionTag": [
+    ],
+    "challengeTag": [
+    ],
+    "ideaTag": [
+    ],
+    "projectTag": [
+    ],
+    "projectMember": [],
+    "messages": [
+      {
+        from: 0,
+        to: 1,
+        text: 'this is a message',
+      },
+      {
+        from: 1,
+        to: 0,
+        text: 'this is a message',
+      },
+      {
+        from: 0,
+        to: 1,
+        text: 'this is a message',
+      },
+      {
+        from: 1,
+        to: 0,
+        text: 'this is a message',
+      },
+      {
+        from: 1,
+        to: 0,
+        text: 'this is a message',
+      }
+    ]
+  }
+
+  for(let i=0; i<USERS; ++i) {
+    data.users.push({
+      "username": "test"+i,
+      "password": "asdfasdf",
+      "email": "test"+i+"@example.com"
+    });
+  }
+
+  return data;
+})();
---
+++ test/functional/notifications/notifications.js
@@ -0,0 +1,80 @@
+'use strict';
+
+let config = require('../partial/config');
+let dbConfig = require('../../../services/db-config');
+let dbData = require('./dbNotifications');
+
+let deps = config.init({db: dbConfig}, dbData);
+let funcs = config.funcs;
+let co = require('co');
+
+describe('notifications', function () {
+  let browserObj = {};
+  let browser;
+
+  let loggedUser = dbData.users[0];
+  let otherUser = dbData.users[1];
+
+  config.beforeTest(browserObj, deps);
+
+  beforeEach(function () {
+    browser = browserObj.Value;
+  });
+
+  describe('/notifications', function () {
+
+    context('not logged in', function () {
+      beforeEach(funcs.logout(browserObj));
+
+      it('should return status 403 not authorized', function (done) {
+        co(function *() {
+          try {
+            yield browser.visit('/notifications');
+          }
+          catch(e) {}
+
+          browser.assert.status(403);
+
+          return done();
+        })
+        .catch(function (err) {
+          return done(err);
+        });
+      });
+    });
+
+    context('logged in', function () {
+      beforeEach(funcs.login(loggedUser, browserObj));
+      afterEach(funcs.logout(browserObj));
+
+      beforeEach(funcs.visit('/notifications', browserObj));
+
+      it('should be successful', function () {
+        browser.assert.success();
+      });
+
+      it('should show list of notifications for logged user', function () {
+        browser.assert.elements('.notification', 5);
+      });
+
+      it('the unprocessed notifications should be highlighted', function () {
+        browser.assert.elements('.last-user-message.unseen', 1);
+      });
+
+      it('when notification is clicked, it should make it viewed and redirect to the right url to process it');
+      it('when notification cross is clicked, it should delete it');
+      it('should show link to edit notification settings');
+    });
+  });
+
+  describe('header of all logged pages', function () {
+    context('logged in', function () {
+      beforeEach(funcs.login(loggedUser, browserObj));
+      afterEach(funcs.logout(browserObj));
+      beforeEach(funcs.visit('/projects', browserObj));
+      it('should show number of unprocessed notifications next to notifications icon', function () {
+        browser.assert.text('.unread-notifications-count', 3);
+      });
+    })
+  });
+});
