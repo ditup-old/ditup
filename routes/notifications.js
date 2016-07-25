d6f7983f05ca351bd83688a00e45d71767d9c9ba
mrkvon <michal.salajka@email.cz>
2016-07-26 02:37:24 +0300

    commenting the code

--- routes/notifications.js
+++ routes/notifications.js
@@ -24,13 +24,17 @@ router.post('/', function (req, res, next) {
   return co(function *() {
     //process notification makes it viewed and redirects to the notification url
     if(req.body['process-notification'] === 'process') {
+      //get the notification id from the form
       let notificationId = req.body['notification-id'];
+      //set notification in database to viewed
       let notification = yield db.notifications.view(notificationId, sessUser.username);
+      //redirect to the url, which notification is pointing to
       return res.redirect(notification.url);
     }
     //delete notification - delete from database and show notification page (in next route)
     else if(req.body['process-notification'] === 'delete') {
       let notificationId = req.body['notification-id'];
+      //deleting notification from database
       let notification = yield db.notifications.remove(notificationId, sessUser.username);
       next();
     }
