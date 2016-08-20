4f0237a5a3b79f41a7926caefcda22913f36f0ba
mrkvon <michal.salajka@email.cz>
2016-08-20 22:59:56 +0200

    setting basic test for project tags

---
+++ test/functional/project/dbTags.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let data = {};
+const USER_NO = 1;
+const TAG_NO = 6;
+const PROJECT_NO = 1;
+
+//adding users
+data.users = [];
+for(let i=0; i<USER_NO; ++i) {
+  data.users.push({
+    username: `test${i}`,
+    password: 'asdfasdf',
+    email: `test${i}@example.com`
+  });
+}
+
+data.tags = [];
+for(let i=0; i<TAG_NO; ++i) {
+  data.tags.push({
+    name: `tag${i}`,
+    description: 'tag description',
+    creator: 0
+  });
+}
+
+data.projects = [];
+for(let i=0; i<PROJECT_NO; ++i) {
+  data.projects.push({
+    name: `project${i}`,
+    description: 'project description',
+    creator: 0,
+    join: true,
+    join_info: 'info for joiners',
+  });
+}
+
+data.projectTag = [];
+for(let i=0; i<TAG_NO-1; ++i) {
+  data.projectTag.push({
+    collection: 0,
+    tag: i,
+    creator: 0
+  });
+}
+
+module.exports = data;
