c0231befb09139c8a6c728fb8d2941da772e1ff5
mrkvon <michal.salajka@email.cz>
2016-09-09 21:50:39 +0200

    more detailed error reporting

--- services/validation/validate.js
+++ services/validation/validate.js
@@ -19,25 +19,20 @@ for(let collection in rules) {
 //this function is doing the validation itself.
 function vldt(value, rules, fieldName) {
   let err = new Error();
+  err.detail = {
+    field: fieldName,
+  }
   err.status = 400;
   for(let rule in rules) {
     if(rule === 'empty') {
       //if the empty: true, field can be empty. if the field is empty, return field as valid.
       if(rules[rule] === true && value === '') return;
     }
-    if(rule === 'regex' || rule === 'regexp') {
-      //validating the value against regexp
-      let regex = new RegExp(rules[rule]);
-      let valid = regex.test(value);
-      if(!valid) {
-        err.message = `the field ${fieldName} must have a form ${rules[rule]}`;
-        throw err;
-      }
-    }
     if(rule === 'minLength') {
       let valid = typeof(value) === 'string' && value.length >= rules.minLength;
       if(!valid) {
         err.message = `the minimal length of ${fieldName || 'field'} is ${rules.minLength}`;
+        err.detail.rule = rule;
         throw err;
       }
     }
@@ -45,6 +40,7 @@ function vldt(value, rules, fieldName) {
       let valid = typeof(value) === 'string' && value.length <= rules.maxLength;
       if(!valid) {
         err.message = `the maximal length of ${fieldName || 'field'} is ${rules.maxLength}`;
+        err.detail.rule = rule;
         throw err;
       }
     }
@@ -52,6 +48,17 @@ function vldt(value, rules, fieldName) {
       let valid = rules[rule].indexOf(value) > -1;
       if(!valid) {
         err.message = `the field ${fieldName} must be a value from the list [${rules[rule].join(', ')}]`;
+        err.detail.rule = rule;
+        throw err;
+      }
+    }
+    if(rule === 'regex' || rule === 'regexp') {
+      //validating the value against regexp
+      let regex = new RegExp(rules[rule]);
+      let valid = regex.test(value);
+      if(!valid) {
+        err.message = `the field ${fieldName} must have a form ${rules[rule]}`;
+        err.detail.rule = 'regex';
         throw err;
       }
     }
